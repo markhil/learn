142930c464d2614d697d360195a018e889c95859
markhil <mark.hil@hotmail.com>
2016-10-27 23:05:12 +0100

    Basic objects

---
+++ contents/objects.js
@@ -0,0 +1,14 @@
+/* Objects
+=======================================================*/
+var obj = {
+	a: "hello world",
+	b: 42,
+	c: true
+};
+
+// Dot notation
+obj.a;
+
+// Bracket notation
+obj["a"];
+
