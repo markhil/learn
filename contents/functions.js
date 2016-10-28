93e3817fb2e4560ab5115a49d596d8f26ac659c7
markaleksander <mark.hil@hotmail.com>
2016-10-28 14:51:53 +0100

    Added IIFE

--- contents/functions.js
+++ contents/functions.js
@@ -7,4 +7,28 @@ var multiply = function(n) {
 
 console.log(multiply(2));
 
-// !! Add more examples !!
+
+// Immediately Invoked Function Expressions
+(function immediate() {
+	console.log("Immediately Invoked Function Expressions");
+})();
+
+
+// IIFE with return value
+var y = (function(){
+	return 5;
+})();
+
+console.log(y);
+
+
+// IIFE with return value
+var x = (function IIFE() {
+	return Math.pow(2,3);
+})();
+
+console.log(x);
+
+
+/* Closures
+=======================================================*/
